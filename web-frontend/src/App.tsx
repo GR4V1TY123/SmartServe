10d8dc3ccbdcc99ee8b111ecb8d5b8a5f658d008
Mandar Ekal <mandarekal367@gmail.com>
2025-10-08 17:40:08 +0530

    init

---
+++ web-frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <span className='text-9xl'>CAt</span>
+      </div>
+    </>
+  )
+}
+
+export default App
